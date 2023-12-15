58d3efb95e5e33a77b9b9e41404eef3e55d5550c
Ayoub Daï <daiayoub59@gmail.com>
2023-12-15 15:44:16 +0100

    :lipstick: Ajout du CSS et des Fichier TailWind :lipstick:

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,20 @@
 <!-- resources/views/index.blade.php -->
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
+    <title>Site Board Game using API </title>
+    @vite('resources/css/app.css')
 </head>
 <body>
-@foreach($data as $d)
-    <div>
-        <p>{{ $d['name'] }}</p>
-        <p> {{$d['image']}}</p>
-        <img src="{{ $d['image'] }}" alt="{{ $d['name'] }}">
-    </div>
-@endforeach
+<div class="container">
+    @foreach($data as $d)
+        <div class="game-card">
+            <p class="game-title">{{ $d['name'] }}</p>
+            <img src="{{ asset($d['image']) }}" alt="Description de l'image" class="game-image">
+        </div>
+    @endforeach
+</div>
 </body>
 </html>
