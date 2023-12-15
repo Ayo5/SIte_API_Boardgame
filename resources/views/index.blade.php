c29f83960b16c65525c77b5663545fc4c35c0bfd
Ayoub Daï <daiayoub59@gmail.com>
2023-12-15 14:38:38 +0100

    :rocket: Ajout de la vue Index Fonctionnel :rocket:

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,18 @@
+<!-- resources/views/index.blade.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
 @foreach($data as $d)
-    <p>{{ $d['name'] }}</p>
+    <div>
+        <p>{{ $d['name'] }}</p>
+        <p> {{$d['image']}}</p>
+        <img src="{{ $d['image'] }}" alt="{{ $d['name'] }}">
+    </div>
 @endforeach
+</body>
+</html>
